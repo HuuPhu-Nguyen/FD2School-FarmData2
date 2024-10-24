078a267cac35ff9c046e58a83e2947858f8f522b
HuuPhu-Nguyen <nguyehuu@dickinson.edu>
2024-10-24 17:20:28 -0400

    merged two detail list for the table  into one single list, tested the detail list

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
@@ -4,10 +4,19 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/e2e")
     })
       
-    it("Check the generateReport button", () => {
+    it("Check the visibility of table header", () => {
         cy.get("[data-cy=table-header]").should("not.exist")
         cy.get("[data-cy=generate-report]").click()
         cy.get("[data-cy=table-header]").should("be.visible")
     }) 
   
+    it("Check the table detail", () => {
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-detail]").should("have.text", "Sample Farm")
+        cy.get("[data-cy=user-detail]").should("have.text", "manager1")
+        cy.get("[data-cy=language-detail]").should("have.text", "English")
+        cy.get("[data-cy=start-date-detail]").should("have.text", "2020-05-05")
+        cy.get("[data-cy=end-date-detail]").should("have.text", "2020-05-15")
+        cy.get("[data-cy=crop-detail]").should("have.text", "ARUGULA")
+    }) 
 })
