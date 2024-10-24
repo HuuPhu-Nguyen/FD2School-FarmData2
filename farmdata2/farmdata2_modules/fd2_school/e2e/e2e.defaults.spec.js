095c8a38e61b3e6d953914c4b38c87563ae8606a
HuuPhu-Nguyen <nguyehuu@dickinson.edu>
2024-10-24 15:29:39 -0400

    tested the crop dropdown

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -16,4 +16,12 @@ describe("Test the harvest report default values", () => {
     cy.get("[data-cy=end-date]").should("have.value", "2020-05-15")
   })
 
+  it("check crop dropdown", ()=>{
+    cy.get("[data-cy=crop-names]").children().eq(0).should("have.value", "ARUGULA")
+    cy.get("[data-cy=crop-names]").children().eq(4).should("have.value", "BEAN-FAVA")
+    cy.get("[data-cy=crop-names]").children().eq(110).should("have.value", "ZUCCHINI")
+    cy.get("[data-cy=crop-names]").children().should("have.length", "111")
+
+  })
+
 })
